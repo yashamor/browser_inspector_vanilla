db42d4f1fbdf93f3d893ee36da05ecd39bf14bb9
yasha <yasha.morer@gmail.com>
2018-12-26 22:46:58 +0200

    save

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 //Globals
 let backgroundColors = {};
 
-document.addEventListener("DOMContentLoaded", function() {
-  //Create wrappers for body content and Inspector
+document.addEventListener("DOMContentLoaded", () => {
+  //Create wrapper for body content
   const bodyWrapper = document.createElement("div");
   bodyWrapper.id = "body";
   while (document.body.firstChild) {
@@ -10,32 +10,48 @@ document.addEventListener("DOMContentLoaded", function() {
   }
   document.body.appendChild(bodyWrapper);
 
-  var inspectorWrapper = document.createElement("div");
+  //Create wrapper for Inspector
+  let inspectorWrapper = document.createElement("div");
+  inspectorWrapper.style.cssText = "display:flex;";
   inspectorWrapper.id = "inspector";
   document.body.appendChild(inspectorWrapper);
-  //Walk through DOM recursevly and fill the Inspector with elements
+
+  //Walk through DOM recursevly and fill the Inspector with elements.
+  //Attach events to connect between Body and Inspector elements.
   renderNodes(
     document.getElementById("body"),
     document.getElementById("inspector")
   );
 });
 
-function renderNodes(node, inspector) {
+const renderNodes = (node, inspector) => {
   if (node.nodeType == Node.ELEMENT_NODE) {
-    const inspectorNode = createInspectorNode(node.nodeName);
-    if (node.parentNode.tagName !== "BODY") {
-      inspectorNode.onmouseover = function(event) {
-        event.stopPropagation();
-        node.style.background = "#198433";
-        node.parentNode.style.background = "#80ed9b";
-      };
+    const inspectorNode =
+      node.parentNode.tagName === "BODY"
+        ? createInspectorNode("BODY")
+        : createInspectorNode(node.nodeName);
 
-      inspectorNode.onmouseout = function(event) {
-        event.stopPropagation();
-        node.style.background = "transparent";
-        node.parentNode.style.background = "transparent";
-      };
-    }
+    //connect mouse events between body and inspector
+    inspectorNode.addEventListener(
+      "mouseover",
+      event => handleMouseOver(event, node),
+      false
+    );
+    inspectorNode.addEventListener(
+      "mouseout",
+      event => handleMouseOut(event, node),
+      false
+    );
+
+    //add drag&drop event handlers
+    inspectorNode.setAttribute("draggable", true);
+    inspectorNode.addEventListener("dragstart", handleDragStart, false);
+    inspectorNode.addEventListener("dragenter", handleDragEnter, false);
+    inspectorNode.addEventListener("dragleave", handleDragLeave, false);
+    inspectorNode.addEventListener("drop", handleDrop, false);
+    inspectorNode.addEventListener("dragend", handleDragEnd, false);
+
+    //append child to parent and replace it.
     inspector.appendChild(appendNodeStyles(inspectorNode));
     inspector = inspectorNode;
 
@@ -43,7 +59,7 @@ function renderNodes(node, inspector) {
       renderNodes(node.children[i], inspectorNode);
     }
   }
-}
+};
 
 const createInspectorNode = name => {
   const inspectorNode = document.createElement("div");
@@ -52,9 +68,10 @@ const createInspectorNode = name => {
   return inspectorNode;
 };
 
+//how can I not return the element and implement element.appendNodeStyles ???
 const appendNodeStyles = element => {
   element.style.cssText =
-    "display:flex; padding:5px; margin:5px; border:1px solid #000";
+    "display:flex; align-self:flex-end; padding:5px; margin:5px; border:1px solid #000;cursor:move";
   if (backgroundColors.hasOwnProperty(element.textContent)) {
     element.style.background = backgroundColors[element.textContent];
   } else {
@@ -65,13 +82,40 @@ const appendNodeStyles = element => {
   return element;
 };
 
-//Helpers
+const handleMouseOver = (event, node) => {
+  event.stopPropagation();
+  node.style.background = "#198433";
+  node.parentNode.style.background = "#80ed9b";
+};
 
+const handleMouseOut = (event, node) => {
+  event.stopPropagation();
+  node.style.background = "none";
+  node.parentNode.style.background = "none";
+};
+
+const handleDragStart = event => {
+  event.target.style.opacity = "0.5";
+};
+const handleDragEnter = event => {
+  event.target.style.borderStyle = "dashed";
+};
+const handleDragLeave = event => {
+  event.target.style.borderStyle = "solid";
+};
+const handleDrop = event => {
+  console.log(event);
+};
+const handleDragEnd = event => {
+  event.target.style.opacity = "1";
+};
+
+//Helpers
 function getRandomColor() {
-  var letters = "0123456789ABCDEF";
-  var color = "#";
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
+  const letters = "BCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * letters.length)];
   }
   return color;
 }
